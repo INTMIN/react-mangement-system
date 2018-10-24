175f6d81394970bc9d31413721fbbbd6731b4598
MIN <https://github.com/INTMIN>
2018-10-24 09:55:13 +0800

    安装styled-components来管理样式

--- src/page/home/index.js
+++ src/page/home/index.js
@@ -18,24 +18,24 @@ class AppIcon extends Component {
   }
 
   render() {
-        return (
-          <Menu
-            onClick={this.handleClick}
-            selectedKeys={[this.state.current]}
-            mode="horizontal"
-          >
-            <Menu.Item key="app">
-              <Icon type="github" theme="outlined" />MIN
-            </Menu.Item>
-            <SubMenu  style={{float: 'right'}} title={<span className="submenu-title-wrapper"><Icon type="github" theme="outlined"/>欢迎管理员</span>}>
-              <MenuItemGroup>
-                <Menu.Item key="setting:1">设置</Menu.Item>
-                <Menu.Item key="setting:2">退出</Menu.Item>
-              </MenuItemGroup>
-            </SubMenu>
-          </Menu>
-        );
-      }
+      return (
+        <Menu
+          onClick={this.handleClick}
+          selectedKeys={[this.state.current]}
+          mode="horizontal"
+        >
+          <Menu.Item key="app">
+            <Icon type="github" theme="outlined" />MIN
+          </Menu.Item>
+          <SubMenu  style={{float: 'right'}} title={<span className="submenu-title-wrapper"><Icon type="github" theme="outlined"/>欢迎管理员</span>}>
+            <MenuItemGroup>
+              <Menu.Item key="setting:1">设置</Menu.Item>
+              <Menu.Item key="setting:2">退出</Menu.Item>
+            </MenuItemGroup>
+          </SubMenu>
+        </Menu>
+      );
+    }
     
   }
 
