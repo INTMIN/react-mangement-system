d3b2f0b482b6edf7950df1b1947b32b0a4e7fd7a
MIN <https://github.com/INTMIN>
2018-10-26 14:55:14 +0800

    登录组件的完成

--- src/page/home/index.js
+++ src/page/home/index.js
@@ -27,15 +27,20 @@ class Home extends Component {
         <Menu.Item key="app">
           <Link to='/'><Icon type="github" theme="outlined" />MIN 管理系统</Link>
         </Menu.Item>
-        <SubMenu  style={{float: 'right'}} title={<span className="submenu-title-wrapper"><Icon type="github" theme="outlined"/>欢迎管理员</span>}>
+        {
+          login ?
+          <SubMenu  style={{float: 'right'}} title={<span className="submenu-title-wrapper"><Icon type="github" theme="outlined"/>欢迎管理员</span>}>
           <MenuItemGroup>
             <Menu.Item key="setting:1">设置</Menu.Item>
-            <Menu.Item key="setting:2" onClick={this.props.logout}>退出</Menu.Item>
+            <Menu.Item key="setting:2" onClick={logout}>退出</Menu.Item>
           </MenuItemGroup>
         </SubMenu>
+        :
         <Menu.Item key="alipay" style={{float: 'right'}}>
         <Link to='/login'><Icon type="user" theme="outlined" />登录</Link>
       </Menu.Item>
+        }
+        
       </Menu>
       <SiderMune />
       </div>
