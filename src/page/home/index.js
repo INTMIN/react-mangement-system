f128b63a52ef4a3fa1649e32c5f03e361eeba40e
MIN <https://github.com/INTMIN>
2018-10-26 14:30:53 +0800

    代码格式调整

--- src/page/home/index.js
+++ src/page/home/index.js
@@ -15,30 +15,29 @@ class Home extends Component {
     }
   }
   render() {
-      return (
-        <div>
-        <Menu
-          onClick={this.handleClick}
-          selectedKeys={[this.state.current]}
-          mode="horizontal"
-        >
-            <Menu.Item key="app">
-            <Link to='/'><Icon type="github" theme="outlined" />MIN</Link>
-          </Menu.Item>
-          
-          <SubMenu  style={{float: 'right'}} title={<span className="submenu-title-wrapper"><Icon type="github" theme="outlined"/>欢迎管理员</span>}>
-            <MenuItemGroup>
-              <Menu.Item key="setting:1">设置</Menu.Item>
-              <Menu.Item key="setting:2">退出</Menu.Item>
-            </MenuItemGroup>
-          </SubMenu>
-          <Menu.Item key="alipay" style={{float: 'right'}}>
-          <Link to='/login'><Icon type="user" theme="outlined" />登录</Link>
+    return (
+      <div>
+      <Menu
+        onClick={this.handleClick}
+        selectedKeys={[this.state.current]}
+        mode="horizontal"
+      >
+        <Menu.Item key="app">
+          <Link to='/'><Icon type="github" theme="outlined" />MIN</Link>
         </Menu.Item>
-        </Menu>
-        <SiderMune />
-        </div>
-      );
+        <SubMenu  style={{float: 'right'}} title={<span className="submenu-title-wrapper"><Icon type="github" theme="outlined"/>欢迎管理员</span>}>
+          <MenuItemGroup>
+            <Menu.Item key="setting:1">设置</Menu.Item>
+            <Menu.Item key="setting:2" onClick={this.state.logout}>退出</Menu.Item>
+          </MenuItemGroup>
+        </SubMenu>
+        <Menu.Item key="alipay" style={{float: 'right'}}>
+        <Link to='/login'><Icon type="user" theme="outlined" />登录</Link>
+      </Menu.Item>
+      </Menu>
+      <SiderMune />
+      </div>
+    );
   }
 }
 
