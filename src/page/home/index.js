ba5cc596d90140196d0652d45b707af98aab577a
MIN <https://github.com/INTMIN>
2018-10-26 14:52:51 +0800

    和redux的连接配置

--- src/page/home/index.js
+++ src/page/home/index.js
@@ -2,6 +2,8 @@ import React, {Component} from 'react';
 import { Menu, Icon } from 'antd';
 import { Link } from 'react-router-dom';
 import SiderMune from '../menu';
+import { connect } from 'react-redux';
+import { actionCreators as loginActionCreators } from '../login/store';
 
 
 const SubMenu = Menu.SubMenu;
@@ -15,20 +17,20 @@ class Home extends Component {
     }
   }
   render() {
+    const { login, logout} = this.props;
     return (
       <div>
       <Menu
-        onClick={this.handleClick}
         selectedKeys={[this.state.current]}
         mode="horizontal"
       >
         <Menu.Item key="app">
-          <Link to='/'><Icon type="github" theme="outlined" />MIN</Link>
+          <Link to='/'><Icon type="github" theme="outlined" />MIN 管理系统</Link>
         </Menu.Item>
         <SubMenu  style={{float: 'right'}} title={<span className="submenu-title-wrapper"><Icon type="github" theme="outlined"/>欢迎管理员</span>}>
           <MenuItemGroup>
             <Menu.Item key="setting:1">设置</Menu.Item>
-            <Menu.Item key="setting:2" onClick={this.state.logout}>退出</Menu.Item>
+            <Menu.Item key="setting:2" onClick={this.props.logout}>退出</Menu.Item>
           </MenuItemGroup>
         </SubMenu>
         <Menu.Item key="alipay" style={{float: 'right'}}>
@@ -40,5 +42,17 @@ class Home extends Component {
     );
   }
 }
+const mapStateToProps = (state) => {
+  return {
+      login: state.getIn(['login','login'])
+  }
+}
+const mapDispatchToProps = (dispatch) => {
+  return {
+      logout() {
+        dispatch(loginActionCreators.logout())
+      }
+  }
+}
 
-export default Home;
+export default connect(mapStateToProps, mapDispatchToProps)(Home);
