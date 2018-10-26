a3f54891678e6e0c306233f04077a5b9eb4334c2
MIN <https://github.com/INTMIN>
2018-10-26 15:30:19 +0800

    第一个图片页面

--- src/page/picture/index.js
+++ src/page/picture/index.js
@@ -1,32 +1,21 @@
-import { List, Card } from 'antd';
+import { Card } from 'antd';
 import React, { Component } from 'react'
 
-
-const data = [
-  {
-    title: 'Title 1',
-  },
-  {
-    title: 'Title 2',
-  },
-  {
-    title: 'Title 3',
-  },
-  {
-    title: 'Title 4',
-  },
-];
+const gridStyle = {
+    width: '30%',
+    textAlign: 'center',
+  };
 class Picture extends Component{
     render(){
         return(
-            <List
-              grid={{ gutter: 16, column: 3 }}
-              dataSource={data}
-              renderItem={item => (
-                <List.Item>
-                  <Card title={item.title}>Card content</Card>
-                </List.Item>
-              )}/>
+            <Card title="分类">
+                <Card.Grid style={gridStyle}>风景</Card.Grid>
+                <Card.Grid style={gridStyle}>生活</Card.Grid>
+                <Card.Grid style={gridStyle}>人文</Card.Grid>
+                <Card.Grid style={gridStyle}>动漫</Card.Grid>
+                <Card.Grid style={gridStyle}>星空</Card.Grid>
+                <Card.Grid style={gridStyle}>壁纸</Card.Grid>
+            </Card>
         )
     }
         
